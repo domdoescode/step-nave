b2117c7570fe11caa67c830de452dd72d962ec19
Dom Udall <dom@synthmedia.co.uk>
2013-12-02 17:46:22 +0000

    Adding version echo's to run script

--- run.sh
+++ run.sh
@@ -9,3 +9,6 @@ sudo npm install -g nave
 sudo nave usemain $WERCKER_NAVE_NODE_VERSION
 
 npm config set tmp $WERCKER_CACHE_DIR/wercker/tmp
+
+echo "node version $(node -v) running"
+echo "npm version $(npm -v) running"
